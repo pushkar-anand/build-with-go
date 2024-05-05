fa89848d1901b5ef0ee247bcbee0fc8706c60c6c
Pushkar Anand <anandpushkar088@gmail.com>
2024-05-06 00:48:20 +0530

    feat: Add environment config reader

---
+++ config/config.go
@@ -0,0 +1,63 @@
+package config
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"os"
+	"strings"
+
+	"github.com/knadh/koanf/parsers/dotenv"
+	"github.com/knadh/koanf/providers/env"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/v2"
+)
+
+const (
+	envTagName = "env"
+)
+
+var (
+	k = koanf.New(".") // global koanf instance
+)
+
+var envKeyModifier = func(s string) string {
+	return strings.ToUpper(s)
+}
+
+func ReadFromEnv[T any](envFile string, prefix string) (*T, error) {
+	err := k.Load(file.Provider(envFile), dotenv.ParserEnv(prefix, ".", envKeyModifier))
+	if err != nil && !fileNotExistsErr(err) {
+		return nil, fmt.Errorf("error loading config from %s: %w", envFile, err)
+	}
+
+	err = k.Load(env.Provider(prefix, ".", envKeyModifier), nil)
+	if err != nil {
+		return nil, fmt.Errorf("error loading config from %s: %w", envFile, err)
+	}
+
+	c, err := unmarshalConfig[T](k)
+	if err != nil {
+		return nil, fmt.Errorf("failed: %w", err)
+	}
+
+	return c, nil
+}
+
+func unmarshalConfig[T any](k *koanf.Koanf) (*T, error) {
+	c := new(T)
+
+	// Unmarshal the whole thing into a struct.
+	err := k.UnmarshalWithConf("", c, koanf.UnmarshalConf{
+		Tag: envTagName,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshalling config: %w", err)
+	}
+
+	return c, nil
+}
+
+func fileNotExistsErr(err error) bool {
+	return errors.Is(err, os.ErrNotExist) || errors.Is(err, fs.ErrNotExist)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,20 @@
 module github.com/pushkar-anand/go-libraries
 
 go 1.22
+
+require (
+	github.com/knadh/koanf/parsers/dotenv v1.0.0
+	github.com/knadh/koanf/providers/env v0.1.0
+	github.com/knadh/koanf/providers/file v0.1.0
+	github.com/knadh/koanf/v2 v2.1.1
+)
+
+require (
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-viper/mapstructure/v2 v2.0.0-alpha.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/knadh/koanf/maps v0.1.1 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	golang.org/x/sys v0.0.0-20220908164124-27713097b956 // indirect
+)
