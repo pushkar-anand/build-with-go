cde1d217016aa0cc2ff8075d899cf73e5e58777d
Pushkar Anand <git@mail.pushkar.dev>
2026-09-05 13:20:25 +0530

    feat(security): add default fallback to hashing options
    Updated `WithTime`, `WithMemory`, `WithParallelism`, `WithSaltLength`, and `WithKeyLength` to use default values when configured with `0`. This ensures consistent behavior and prevents misconfigurations while maintaining flexibility with override capabilities.

--- security/password/options.go
+++ security/password/options.go
@@ -22,38 +22,58 @@ func WithPepper(pepper string) Option {
 	})
 }
 
-// WithTime sets the argon2id time (iteration) parameter.
+// WithTime sets the argon2id time (iteration) parameter. 0 means default.
 func WithTime(time uint32) Option {
 	return optionFunc(func(h *Hasher) {
+		if time == 0 {
+			time = defaultTime
+		}
+
 		h.time = time
 	})
 }
 
-// WithMemory sets the argon2id memory parameter, in KiB.
+// WithMemory sets the argon2id memory parameter, in KiB. 0 means default.
 func WithMemory(memory uint32) Option {
 	return optionFunc(func(h *Hasher) {
+		if memory == 0 {
+			memory = defaultMemory
+		}
+
 		h.memory = memory
 	})
 }
 
-// WithParallelism sets the argon2id parallelism (thread count) parameter.
+// WithParallelism sets the argon2id parallelism (thread count) parameter. 0 means default.
 func WithParallelism(parallelism uint8) Option {
 	return optionFunc(func(h *Hasher) {
+		if parallelism == 0 {
+			parallelism = defaultParallelism
+		}
+
 		h.parallelism = parallelism
 	})
 }
 
 // WithSaltLength sets the length, in bytes, of the random salt generated for
-// each hash.
+// each hash. 0 means default.
 func WithSaltLength(length uint32) Option {
 	return optionFunc(func(h *Hasher) {
+		if length == 0 {
+			length = defaultSaltLength
+		}
+
 		h.saltLength = length
 	})
 }
 
-// WithKeyLength sets the length, in bytes, of the derived key.
+// WithKeyLength sets the length, in bytes, of the derived key. 0 means default.
 func WithKeyLength(length uint32) Option {
 	return optionFunc(func(h *Hasher) {
+		if length == 0 {
+			length = defaultKeyLength
+		}
+
 		h.keyLength = length
 	})
 }
